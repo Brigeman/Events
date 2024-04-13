e1966e1b284bc534931169e362f80108293e1366
Brigeman <yurgen.brigeman@mail.ru>
2024-04-13 02:42:18 +0200

    logging

--- events/serializers.py
+++ events/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Event, Registration
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
--- events/views.py
+++ events/views.py
@@ -2,6 +2,34 @@ from .models import Event, Registration
 from .serializers import EventSerializer, UserRegistrationSerializer, RegistrationSerializer
 from rest_framework import status, views, viewsets, permissions
 from rest_framework.response import Response
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+    def create(self, request, *args, **kwargs):
+        logger.debug(f"Creating event with data: {request.data}")
+        return super().create(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        logger.debug("Listing all events")
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        logger.debug(f"Retrieving event with id: {pk}")
+        return super().retrieve(request, pk, *args, **kwargs)
+
+    def update(self, request, pk=None, *args, **kwargs):
+        logger.debug(f"Updating event with id: {pk} with data: {request.data}")
+        return super().update(request, pk, *args, **kwargs)
+
+    def destroy(self, request, pk=None, *args, **kwargs):
+        logger.debug(f"Deleting event with id: {pk}")
+        return super().destroy(request, pk, *args, **kwargs)
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -38,3 +66,11 @@ class EventRegistrationView(views.APIView):
         registration.status = 'cancelled'
         registration.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
